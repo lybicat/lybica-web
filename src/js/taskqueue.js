be632f5109e55433079192217fc1e9fc67439fc5
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-06 14:03:43 +0800

    get queued tasks from api

--- src/js/taskqueue.js
+++ src/js/taskqueue.js
@@ -1,40 +1,23 @@
-'use strict';
-
 var $ = require('jquery');
 
 var TaskQueue = {
     render: function(container) {
-        var data = [{status: 'pending',
-            build: 'TL15A_123414_23444',
-            plan: 'CRT SISO',
-            device: 'TL1501',
-            triggerby: 'QT1',
-            triggerat: '3hours ago'},
-            {status: 'running',
-            build: 'TL15A_123412_23422',
-            plan: 'CRT RRM',
-            device: 'TL1509',
-            triggerby: 'QT2',
-            triggerat: '1hour ago'},
-            {status: 'running',
-            build: 'TL15A_123412_23422',
-            plan: 'CRT TRM',
-            device: 'TL1510',
-            triggerby: 'QT2',
-            triggerat: '1hour ago'}];
+        $.getJSON('/api/tasks', function(data) {
+            var tableRows = data.map(function(d) {
+                var statusTxt = d.started && 'running' || 'pending';
+                return '<tr>' +
+                       '  <td><span class="ci-status ci-' + statusTxt + '">' + '<i class="fa ' + (d.started && 'fa-refresh' || 'fa-clock-o') + '"></i>' + statusTxt + '</span></td>' +
+                       '  <td><a class="ci-build" href="">' + d.build + '</a></td>' +
+                       '  <td><a class="ci-plan">' + (d.plan || 'PILOT') + '</a></td>' +
+                       '  <td><a class="ci-device">' + d.device.join(';') + '</a></td>' +
+                       '  <td><a class="ci-trigger">' + d.triggerby + '</a></td>' +
+                       '  <td><a class="ci-time">' + d.triggerat + '</a></td>' +
+                       '  <td><a class="ci-more"><i class="fa fa-external-link"></i></a></td>' +
+                       '</tr>';
+            }).join('');
 
-        var tableRows = data.map(function(d) {
-            return '<tr>' +
-              '  <td><span class="ci-status ci-' + d.status + '">' + '<i class="fa ' + (d.status === 'pending' && 'fa-clock-o' || 'fa-refresh') + '"></i>' + d.status + '</span></td>' +
-              '  <td><a class="ci-build" href="">' + d.build + '</a></td>' +
-              '  <td><a class="ci-plan">' + d.plan + '</a></td>' +
-              '  <td><a class="ci-device">' + d.device + '</a></td>' +
-              '  <td><a class="ci-trigger">' + d.triggerby + '</a></td>' +
-              '  <td><a class="ci-time">' + d.triggerat + '</a></td>' +
-              '</tr>';
-        }).join('');
-
-        $(container).append(tableRows);
+            $(container).append(tableRows);
+        });
     }
 };
 
