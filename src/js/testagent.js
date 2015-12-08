962729718d3bc3bc7b5d0c6c8aa625d05d0198c4
Zhang Yu <yu.3.zhang@nokia.com>
2015-12-08 11:24:19 +0800

    get agents remotely

--- src/js/testagent.js
+++ src/js/testagent.js
@@ -2,13 +2,6 @@ var $ = require('jquery');
 
 var TestAgent = {
   render: function(container) {
-    var data = [
-      {available: false, name: 'my-laptop', ip: '127.0.0.1', runners: {all: 2, running: 0}, labels: ['linux', 'node', 'python']},
-      {available: true, name: 'my-desktop', ip: '127.0.0.2', runners: {all: 5, running: 1}, labels: ['windows', 'c#', 'python']},
-      {available: true, name: 'test-workstation', ip: '127.0.0.3', runners: {all: 10, running: 0}, labels: ['linux', 'node', 'nginx', 'docker', 'python']},
-      {available: true, name: 'second-workstation', ip: '127.0.0.4', runners: {all: 10, running: 10}, labels: ['linux', 'node', 'nginx', 'docker', 'python']},
-    ];
-
     function _getStatusTxt(d) {
       if (d.available === false)
         return 'offline';
@@ -26,22 +19,24 @@ var TestAgent = {
       unavailable: '<i class="fa fa-battery-empty"></i>'
     };
 
-    var tableRows = data.map(function(d) {
-      var statusTxt = _getStatusTxt(d);
-      return '<tr>' +
-             '  <td><a class="agent-status agent-' + statusTxt + '">' + statusIcon[statusTxt] + statusTxt + '</td>' +
-             '  <td><a class="agent-name">' + d.name + '</a></td>' +
-             '  <td><a class="agent-ip">' + d.ip + '</a></td>' +
-             '  <td><a class="agent-runners">' + d.runners.running + '/' + d.runners.all + '</a></td>' +
-             '  <td><a class="agent-labels">' +
-             d.labels.map(function(label) {
-                 return '<span class="tag label label-default">' + label + '</span>';
-             }).join('') + '</a></td>' +
-             '  <td><a class="agent-action"><i class="fa fa-pencil-square-o"></i></a></td>' +
-             '</tr>';
-    }).join('');
+    $.getJSON('/api/agents', function(data) {
+      var tableRows = data.map(function(d) {
+        var statusTxt = _getStatusTxt(d);
+        return '<tr>' +
+               '  <td><a class="agent-status agent-' + statusTxt + '">' + statusIcon[statusTxt] + statusTxt + '</td>' +
+               '  <td><a class="agent-name">' + d.name + '</a></td>' +
+               '  <td><a class="agent-ip">' + d.ip + '</a></td>' +
+               '  <td><a class="agent-runners">' + d.runners.running + '/' + d.runners.all + '</a></td>' +
+               '  <td><a class="agent-labels">' +
+               d.labels.map(function(label) {
+                   return '<span class="tag label label-default">' + label + '</span>';
+               }).join('') + '</a></td>' +
+               '  <td><a class="agent-action"><i class="fa fa-pencil-square-o"></i></a></td>' +
+               '</tr>';
+      }).join('');
 
-    $(container).append(tableRows);
+      $(container).append(tableRows);
+    });
   }
 };
 
