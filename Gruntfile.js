c44d2b36dd969b14544bd1bab413ecf3e5e1a7ef
Zhang Yu <yu.3.zhang@nokia.com>
2015-12-21 15:45:59 +0800

    use different css for testplan and testscheduler page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,70 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        jade: {
-            compile: {
-                files: {
-                    'dist/index.html': 'src/jade/index.jade',
-                    'dist/testplan.html': 'src/jade/testplan.jade',
-                    'dist/resourcemgr.html': 'src/jade/resourcemgr.jade',
-                    'dist/testreport.html': 'src/jade/testreport.jade',
-                    'dist/swbuild.html': 'src/jade/swbuild.jade',
-                    'dist/testscheduler.html': 'src/jade/testscheduler.jade',
-                    'dist/testagent.html': 'src/jade/testagent.jade',
-                    'dist/console.html': 'src/jade/console.jade'
-                }
-            }
+  grunt.initConfig({
+    stylus: {
+      compile: {
+        options: {
+          paths: ['node_modules'],
+          "include css": true
         },
-        browserify: {
-            compile: {
-                files: {
-                    'dist/js/taskqueue.js': 'src/js/taskqueue.js',
-                    'dist/js/testplan.js': 'src/js/testplan.js',
-                    'dist/js/resourcemgr.js': 'src/js/resourcemgr.js',
-                    'dist/js/testreport.js': 'src/js/testreport.js',
-                    'dist/js/swbuild.js': 'src/js/swbuild.js',
-                    'dist/js/testscheduler.js': 'src/js/testscheduler.js',
-                    'dist/js/testagent.js': 'src/js/testagent.js',
-                    'dist/js/includes/socket.js': 'src/js/includes/socket.js',
-                    'dist/js/includes/utils.js': 'src/js/includes/utils.js',
-                    'dist/js/console.js': 'src/js/console.js'
-                }
-            }
-        },
-        uglify: {
-            compile: {
-                files: {
-                    'dist/js/taskqueue.js': 'dist/js/taskqueue.js',
-                    'dist/js/testplan.js': 'dist/js/testplan.js',
-                    'dist/js/resourcemgr.js': 'dist/js/resourcemgr.js',
-                    'dist/js/testreport.js': 'dist/js/testreport.js',
-                    'dist/js/swbuild.js': 'dist/js/swbuild.js',
-                    'dist/js/testscheduler.js': 'dist/js/testscheduler.js',
-                    'dist/js/testagent.js': 'dist/js/testagent.js',
-                    'dist/js/includes/socket.js': 'dist/js/includes/socket.js',
-                    'dist/js/includes/utils.js': 'dist/js/includes/utils.js',
-                    'dist/js/console.js': 'dist/js/console.js'
-                }
-            }
+        files: {
+          'dist/css/main.css': 'src/css/main.styl',
+          'dist/css/testplan.css': 'src/css/testplan.styl',
+          'dist/css/testscheduler.css': 'src/css/testscheduler.styl',
         }
-    });
+      }
+    },
+    jade: {
+      compile: {
+        files: {
+          'dist/index.html': 'src/jade/index.jade',
+          'dist/testplan.html': 'src/jade/testplan.jade',
+          'dist/resourcemgr.html': 'src/jade/resourcemgr.jade',
+          'dist/testreport.html': 'src/jade/testreport.jade',
+          'dist/swbuild.html': 'src/jade/swbuild.jade',
+          'dist/testscheduler.html': 'src/jade/testscheduler.jade',
+          'dist/testagent.html': 'src/jade/testagent.jade',
+          'dist/console.html': 'src/jade/console.jade'
+        }
+      }
+    },
+    browserify: {
+      compile: {
+        files: {
+          'dist/js/taskqueue.js': 'src/js/taskqueue.js',
+          'dist/js/testplan.js': 'src/js/testplan.js',
+          'dist/js/resourcemgr.js': 'src/js/resourcemgr.js',
+          'dist/js/testreport.js': 'src/js/testreport.js',
+          'dist/js/swbuild.js': 'src/js/swbuild.js',
+          'dist/js/testscheduler.js': 'src/js/testscheduler.js',
+          'dist/js/testagent.js': 'src/js/testagent.js',
+          'dist/js/includes/socket.js': 'src/js/includes/socket.js',
+          'dist/js/includes/utils.js': 'src/js/includes/utils.js',
+          'dist/js/console.js': 'src/js/console.js'
+        }
+      }
+    },
+    uglify: {
+      compile: {
+        files: {
+          'dist/js/taskqueue.js': 'dist/js/taskqueue.js',
+          'dist/js/testplan.js': 'dist/js/testplan.js',
+          'dist/js/resourcemgr.js': 'dist/js/resourcemgr.js',
+          'dist/js/testreport.js': 'dist/js/testreport.js',
+          'dist/js/swbuild.js': 'dist/js/swbuild.js',
+          'dist/js/testscheduler.js': 'dist/js/testscheduler.js',
+          'dist/js/testagent.js': 'dist/js/testagent.js',
+          'dist/js/includes/socket.js': 'dist/js/includes/socket.js',
+          'dist/js/includes/utils.js': 'dist/js/includes/utils.js',
+          'dist/js/console.js': 'dist/js/console.js'
+        }
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['jade', 'browserify', 'uglify']);
+  grunt.registerTask('default', ['jade', 'stylus', 'browserify', 'uglify']);
 };
