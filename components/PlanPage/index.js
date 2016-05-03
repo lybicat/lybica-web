c5c83ccc2b4cbe43ef26b62da3860504510037d8
Zhang Yu <yu.3.zhang@nokia.com>
2016-05-03 16:58:28 +0800

    rename searchInput to SearchForm

--- components/PlanPage/index.js
+++ components/PlanPage/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 import { Layout, Fixed, Flex } from 'react-layout-pane'
 import $ from 'jquery'
 import Loading from 'react-loading'
-import SearchInput from '../searchInput'
+import SearchForm from '../SearchForm'
 
 
 export default React.createClass({
@@ -30,12 +30,15 @@ export default React.createClass({
     return (
       <Layout type="row">
         <Fixed className="sidebar">
-          <SearchInput placeholder="Plan Name, author..."/>
+          <SearchForm placeholder="Plan Name, author..."/>
           {this.state.loaded === false
             ? <Loading type="bars" color='#e3e3e3'/>
             : <ul>
                 {this.state.plans.map(function(p) {
-                  return <li key={p._id}><Link activeClassName="activeItem" to={`/plans/${p._id}`}>{p.name}</Link></li>
+                  return (
+                    <li key={p._id}>
+                      <Link activeClassName="activeItem" to={`/plans/${p._id}`}>{p.name}</Link>
+                    </li>)
                 })}
               </ul>
           }
