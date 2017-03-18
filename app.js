946979f902bf0857b0a4bf297189ba110be7faf5
zhang yu <feiyuw@gmail.com>
2017-03-18 17:42:20 +0800

    for eslint

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import { browserHistory, Router, Route, IndexRoute } from 'react-router'
-import { Layout, Flex } from 'react-layout-pane'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { browserHistory, Router, Route, IndexRoute } from 'react-router';
+import { Layout, Flex } from 'react-layout-pane';
 
-import NavBar from './components/NavBar'
-import DashboardPage from './components/DashboardPage'
-import TaskPage from './components/TaskPage'
-import TaskDetail from './components/TaskPage/TaskDetail'
-import PlanPage from './components/PlanPage'
-import PlanDetail from './components/PlanPage/PlanDetail'
-import SchedulePage from './components/SchedulePage'
-import TriggerPage from './components/TriggerPage'
-import ReleasePage from './components/ReleasePage'
-import AgentPage from './components/AgentPage'
+import NavBar from './components/NavBar';
+import DashboardPage from './components/DashboardPage';
+import TaskPage from './components/TaskPage';
+import TaskDetail from './components/TaskPage/TaskDetail';
+import PlanPage from './components/PlanPage';
+import PlanDetail from './components/PlanPage/PlanDetail';
+import SchedulePage from './components/SchedulePage';
+import TriggerPage from './components/TriggerPage';
+import ReleasePage from './components/ReleasePage';
+import AgentPage from './components/AgentPage';
 
 
 const menus = [
-  {name: "Home", icon: "home", url: "/", onlyActiveOnIndex: true},
-  {name: "Tasks", icon: "tasks", url: "/tasks"},
-  {name: "Plans", icon: "book", url: "/plans"},
-  {name: "Schedules", icon: "calendar", url: "/schedules"},
-  {name: "Triggers", icon: "bolt", url: "/triggers"},
-  {name: "Releases", icon: "diamond", url: "/releases"},
-  {name: "Agents", icon: "desktop", url: "/agents"},
+  {name: 'Home', icon: 'home', url: '/', onlyActiveOnIndex: true},
+  {name: 'Tasks', icon: 'tasks', url: '/tasks'},
+  {name: 'Plans', icon: 'book', url: '/plans'},
+  {name: 'Schedules', icon: 'calendar', url: '/schedules'},
+  {name: 'Triggers', icon: 'bolt', url: '/triggers'},
+  {name: 'Releases', icon: 'diamond', url: '/releases'},
+  {name: 'Agents', icon: 'desktop', url: '/agents'},
 ];
 
 
@@ -37,7 +37,7 @@ class App extends Component{
       </Layout>
     );
   }
-};
+}
 
 
 render((
